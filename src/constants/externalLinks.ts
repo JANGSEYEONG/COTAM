9343ba469907c5dec89b0de1451d8942636441e6
JANGSEYEONG <jang.se.yeong000@gmail.com>
2025-05-26 04:35:47 +0000

    chore: trivago 플러그인 업데이트

--- src/constants/externalLinks.ts
+++ src/constants/externalLinks.ts
@@ -1,5 +1,6 @@
 export const KAKAO_OPEN_CHAT_URL = 'https://open.kakao.com/o/srJ7wcje';
-export const RECRUITMENT_FORM_URL = 'https://forms.gle/p9gGiz6b735FffsaA';
+export const RECRUITMENT_FORM_URL =
+  'https://docs.google.com/forms/d/e/1FAIpQLSeTh02hzIUIfkrPPf91acDecHNbgNMIcoSpSHmz88zgDg3yVQ/viewform?usp=sharing&ouid=116088795754904089384';
 
 export const STUDY_LEADER_LINKEDIN_URL =
   'https://www.linkedin.com/in/%EC%8A%B9%EB%AA%A8-%EA%B9%80-848b2b242/?originalSubdomain=kr';
